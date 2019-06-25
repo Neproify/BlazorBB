f1ba361873f1e4c602b4b971858c744631d3d00a
Neproify <yamasaki3214@gmail.com>
2019-06-25 22:52:11 +0200

    Initial code

---
+++ BlazorBB.Server/Controllers/ForumController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BlazorBB.Server.Controllers
+{
+    public class ForumController : Controller
+    {
+        // GET: /api/forums/all
+        [HttpGet("/api/forums/all", Name = "Forums/All")]
+        public IActionResult All()
+        {
+            //TODO
+
+            List<Shared.Models.Forum> forums = new List<Shared.Models.Forum>();
+
+            forums.Add(new Shared.Models.Forum { Id = MongoDB.Bson.ObjectId.GenerateNewId(), name = "Test 1" });
+            forums.Add(new Shared.Models.Forum { Id = MongoDB.Bson.ObjectId.GenerateNewId(), name = "Test 2" });
+            forums.Add(new Shared.Models.Forum { Id = MongoDB.Bson.ObjectId.GenerateNewId(), name = "Test 3" });
+            forums.Add(new Shared.Models.Forum { Id = MongoDB.Bson.ObjectId.GenerateNewId(), name = "Test 4" });
+
+            return Json(forums);
+        }
+    }
+}
---
+++ BlazorBB.Shared/ObjectIdConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using MongoDB.Bson;
+
+namespace BlazorBB.Shared
+{
+    public class ObjectIdConverter : JsonConverter
+    {
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var stringId = serializer.Deserialize<string>(reader);
+            return ObjectId.Parse(stringId);
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            serializer.Serialize(writer, value);
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
